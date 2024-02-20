283a55131a2ffa6d5f4f6e4cbdd1641a1474b9b5
lishunyang <lishunyang@pku.edu.cn>
2024-02-20 19:37:24 +0800

    feat: ✨ change intro header

--- src/app/_components/container.tsx
+++ src/app/_components/container.tsx
@@ -3,7 +3,7 @@ type Props = {
 };
 
 const Container = ({ children }: Props) => {
-  return <div className="container mx-auto px-5">{children}</div>;
+  return <div className="max-md:container md:max-w-3xl mx-auto px-5">{children}</div>;
 };
 
 export default Container;
--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -1,13 +1,16 @@
+import Image from "next/image";
+
+import githubIcon from '@/public/assets/images/github-mark.svg';
 import { SITE_NAME } from "@/lib/constants";
 
 export function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+      <h1 className="text-4xl md:text-6xl font-bold tracking-tighter leading-tight md:pr-8">
         {SITE_NAME}
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        {/* TODO: Add something here */}
+        <Image src={githubIcon} width={24} height={24} alt="github icon" />
       </h4>
     </section>
   );
