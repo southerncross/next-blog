3229bc700433c26097db0cd4bca264da8ea26326
lishunyang <lishunyang@mokahr.com>
2024-02-22 00:16:28 +0800

    feat: ✨ Optimize styles of links and code blocks in markdown.

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,7 +1,16 @@
 import { remark } from "remark";
-import html from "remark-html";
+import remarkRehype from "remark-rehype";
+import rehypeStringify from "rehype-stringify";
+import rehypePrettyCode from "rehype-pretty-code";
+
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown);
+  const result = await remark()
+    .use(remarkRehype)
+    .use(rehypePrettyCode, {
+      theme: "github-light"
+    })
+    .use(rehypeStringify)
+    .process(markdown);
   return result.toString();
 }
