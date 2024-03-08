ee3eac84ee38cab8122759844562e683cb62e96a
lishunyang <lishunyang@mokahr.com>
2024-03-08 11:22:11 +0800

    wip commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import Container from '../_components/container';
+import { Intro } from '../_components/intro';
+
+export default function About() {
+  return (
+    <main>
+      <Container>
+        <Intro />
+      </Container>
+    </main>
+  );
+}
