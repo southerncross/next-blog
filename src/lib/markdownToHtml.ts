5425ee6c85bd4c22032be1e3dfd34b68df7df8c9
lishunyang <lishunyang@mokahr.com>
2024-04-15 12:05:42 +0800

    feat: :sparkles: Add anchor link for markdown header.

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,7 +1,10 @@
+import { h } from 'hastscript';
 import { remark } from 'remark';
 import remarkRehype from 'remark-rehype';
 import rehypeStringify from 'rehype-stringify';
 import rehypePrettyCode from 'rehype-pretty-code';
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 
 export default async function markdownToHtml(markdown: string) {
   const result = await remark()
@@ -10,6 +13,8 @@ export default async function markdownToHtml(markdown: string) {
       theme: 'github-dark-dimmed',
       defaultLang: 'ansi',
     })
+    .use(rehypeSlug)
+    .use(rehypeAutolinkHeadings, { content: () => h('span.mr-2', '#') })
     .use(rehypeStringify)
     .process(markdown);
   return result.toString();
