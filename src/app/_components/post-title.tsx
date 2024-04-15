b64a87564752e6a0651f9684765786c7992ac54d
lishunyang <lishunyang@mokahr.com>
2024-04-15 10:04:35 +0800

    fix: :bug: Fix the problem that the font-size of post title is smaller than h1.

--- src/app/_components/post-title.tsx
+++ src/app/_components/post-title.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export function PostTitle({ children }: Props) {
   return (
-    <h1 className="mb-12 text-center text-4xl font-bold leading-tight tracking-tighter md:text-left md:text-5xl md:leading-none lg:text-6xl">
+    <h1 className="mb-12 text-center text-5xl font-bold leading-tight tracking-tighter md:text-left md:text-6xl md:leading-none lg:text-7xl">
       {children}
     </h1>
   );
